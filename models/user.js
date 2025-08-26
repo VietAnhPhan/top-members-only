e44c60825b9046841c62055d2a42707f84ff9920
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-08-26 17:05:17 +0700

    Reapply "set up for users and establish database connection"
    This reverts commit c716176e177a5de35377ea5032cb0b0a16ffc483.

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const pool = require("../db/pool");
+
+async function getAllUsers() {
+  const { rows } = await pool.query("SELECT * FROM users");
+
+  return rows;
+}
+
+async function getUserByUsername(username) {
+  const { rows } = await pool.query(
+    "SELECT * FROM users WHERE user_name = ($)1",
+    [username]
+  );
+
+  return rows[0];
+}
+
+async function createUser(user) {
+  await pool.query(
+    "INSERT INTO users (first_name, last_name, user_name, password) VALUES (($1), ($2), ($3), ($4))",
+    [user.first_name, user.last_name, user.user_name, user.password]
+  );
+}
+
+async function updateUser(user) {
+  await pool.query(
+    "UPDATE users SET first_name = ($1), last_name = ($2), password = ($3))",
+    [user.first_name, user.last_name, user.password]
+  );
+}
+
+async function deleteUser(user) {
+  await pool.query("DELETE users WHERE user_name = ($1)", [user.user_name]);
+}
+
+module.exports = {
+  getAllUsers,
+  getUserByUsername,
+  createUser,
+  updateUser,
+  deleteUser,
+};
